510da22520a1b85e16e8d24b8cbf275aef5a6ad9
boom-e-rang <ola@u.northwestern.edu>
2017-12-04 19:49:07 -0600

    ik solver working

--- scripts/gripping_and_twisting.py
+++ scripts/gripping_and_twisting.py
@@ -6,19 +6,26 @@ from sensor_msgs.msg import Range
 from baxter_core_msgs.msg import JointCommand
 from baxter_core_msgs.msg import EndEffectorCommand
 from std_srvs.srv import SetBool
+from sensor_msgs.msg import JointState
 
-class Gripping():
+class Opening():
     def __init__(self):
         self.distance = 100
-        rospy.Subscriber("robot/range/left_hand_range/state", Range, self.callback)
+        rospy.Subscriber("robot/range/left_hand_range/state", Range, self.callback_IR)
 
         self.pub_grip = rospy.Publisher('robot/end_effector/left_gripper/command', EndEffectorCommand, queue_size=10)
         self.pub_turn = rospy.Publisher('robot/limb/left/joint_command', JointCommand, queue_size=10)
 
+        rospy.Subscriber('robot/ref_joint_states', JointState, self.callback_states)
+        self.current_states = JointState()
 
-    def callback(self, data):
+    def callback_IR(self, data):
         self.distance = data.range
-        #rospy.loginfo("I'm %f above the lid!", self.distance)
+        # rospy.loginfo("I'm %f above the lid!", self.distance)
+
+    def callback_states(self, data):
+        self.current_states = data
+        # rospy.loginfo("I'm %f above the lid!", self.distance)
 
     def grip_cup(self):
         self.grip = EndEffectorCommand()
@@ -39,27 +46,31 @@ class Gripping():
     def twist_CCW(self):
         #unscrew cup
         self.turn = JointCommand()
-        self.turn.mode = 3 # position control (1)
+        self.turn.mode = 1 # position control (1)
         self.turn.names = ['left_w2']
-        self.turn.command = [-30]
+        self.turn.command = [-3]
         self.pub_turn.publish(self.turn)
         rospy.loginfo("Twisting")
+        rospy.sleep(1)
+        self.pub_turn.publish(self.turn)
         rospy.sleep(1)
 
     def twist_CW(self):
         #untwist gripper
         self.turn = JointCommand()
-        self.turn.mode = 3 # position control (1)
+        self.turn.mode = 1 # position control (1)
         self.turn.names = ['left_w2']
-        self.turn.command = [30]
+        self.turn.command = [3]
         self.pub_turn.publish(self.turn)
         rospy.loginfo("Untwisting")
+        rospy.sleep(1)
+        self.pub_turn.publish(self.turn)
         rospy.sleep(1)
 
 def main(input):
     if (input.data == 1):
         rospy.sleep(2)
-        grip_class = Gripping()
+        grip_class = Opening()
 
         # while (distance> 0.1)
             # move arm
@@ -67,8 +78,10 @@ def main(input):
         # rospy.loginfo("DISTANCE: %f", grip_class.distance)
         # if (grip_class.distance < 0.15):
 
+        null_state = JointState()
+        null_state = grip_class.current_states
         count = 0
-        while (count<4):
+        while (count<3):
             grip_class.release_cup()
             grip_class.twist_CW()
             grip_class.grip_cup()
---
+++ scripts/move_ola.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python
+
+import argparse
+import struct
+import sys
+
+import rospy
+from math import pi
+
+import baxter_interface
+
+from tf.transformations import quaternion_from_euler
+from tf.transformations import quaternion_multiply
+
+from geometry_msgs.msg import (
+    PoseStamped,
+    Pose,
+    Point,
+    Quaternion,
+)
+from std_msgs.msg import Header
+
+from baxter_core_msgs.srv import (
+    SolvePositionIK,
+    SolvePositionIKRequest,
+)
+
+class IK_solver():
+    def __init__(self):
+        rospy.init_node("rsdk_ik_service_client")
+        self.poses = {
+            'left': PoseStamped(
+                header=Header(stamp=rospy.Time.now(), frame_id='base'),
+                pose=Pose(
+                    position=Point(
+                        x=0.657579481614,
+                        y=0.851981417433,
+                        z=0.0388352386502,
+                    ),
+                    orientation=Quaternion(
+                        x=-0.366894936773,
+                        y=0.885980397775,
+                        z=0.108155782462,
+                        w=0.262162481772,
+                    ),
+                ),
+            )
+        }
+        self.flag = 0
+
+    def ik_test(self, limb):
+
+        ns = "ExternalTools/" + limb + "/PositionKinematicsNode/IKService"
+        iksvc = rospy.ServiceProxy(ns, SolvePositionIK)
+        self.ikreq = SolvePositionIKRequest()
+        hdr = Header(stamp=rospy.Time.now(), frame_id='base')
+
+        # example = PoseStamped(
+        #     header=Header(stamp=rospy.Time.now(), frame_id='base'),
+        #     pose=Pose(
+        #         position=Point(
+        #             x=0.657579481614,
+        #             y=0.851981417433,
+        #             z=0.0388352386502,
+        #         ),
+        #         orientation=Quaternion(
+        #             x=-0.366894936773,
+        #             y=0.885980397775,
+        #             z=0.108155782462,
+        #             w=0.262162481772,
+        #         ),
+        #     )
+        # )
+        self.ikreq.pose_stamp.append(self.poses[limb])
+        try:
+            rospy.wait_for_service(ns, 5.0)
+            resp = iksvc(self.ikreq)
+        except (rospy.ServiceException, rospy.ROSException), e:
+            rospy.logerr("Service call failed: %s" % (e,))
+            return 1
+
+        # Check if result valid, and type of seed ultimately used to get solution
+        # convert rospy's string representation of uint8[]'s to int's
+        # resp_seeds = struct.unpack('<%dB' % len(resp.result_type),
+        #                            resp.result_type)
+        # if (resp_seeds[0] != resp.RESULT_INVALID):
+        #     seed_str = {
+        #                 self.ikreq.SEED_USER: 'User Provided Seed',
+        #                 self.ikreq.SEED_CURRENT: 'Current Joint Angles',
+        #                 self.ikreq.SEED_NS_MAP: 'Nullspace Setpoints',
+        #                }.get(resp_seeds[0], 'None')
+        #     print("SUCCESS - Valid Joint Solution Found from Seed Type: %s" %
+        #           (seed_str,))
+        #     # Format solution into Limb API-compatible dictionary
+        #
+        #     #self.left.move_to_joint_positions(limb_joints)
+        #
+        #     print "\nIK Joint Solution:\n", self.limb_joints
+        #     print "------------------"
+        #     print "Response Message:\n", resp
+        # else:
+        #     print("INVALID POSE - No Valid Joint Solution Found.")
+        #     self.flag = 0;
+
+        self.limb_joints = dict(zip(resp.joints[0].name, resp.joints[0].position))
+        self.left_var = baxter_interface.Limb('left')
+        return 0
+
+    def callback(self, data):
+        if (1):
+            q_rot = quaternion_from_euler(0,pi,0)
+            q_desired = [data.orientation.x, data.orientation.y, data.orientation.z, data.orientation.w]
+            print(q_desired)
+            q_new = quaternion_multiply(q_rot, q_desired)
+            new_pose = data
+            new_pose.orientation.x = q_new[0]
+            new_pose.orientation.y = q_new[1]
+            new_pose.orientation.z = q_new[2]
+            new_pose.orientation.w = q_new[3]
+            self.poses = {
+                'left': PoseStamped(
+                    header=Header(stamp=rospy.Time.now(), frame_id='base'),
+                    pose=new_pose
+                    )
+            }
+            print("I got a new pose. Yayy!")
+
+            # find IK solution
+            arg_fmt = argparse.RawDescriptionHelpFormatter
+            parser = argparse.ArgumentParser(formatter_class=arg_fmt,
+                                             description=main.__doc__)
+            parser.add_argument(
+                '-l', '--limb', choices=['left'], required=True,
+                help="the limb to test"
+            )
+            args = parser.parse_args(rospy.myargv()[1:])
+            self.ik_test('left')
+            if (self.flag != 1):
+                self.left_var.move_to_joint_positions(self.limb_joints)
+                print("I moved to the desired pose. Yayy!")
+                rospy.sleep(5)
+            else:
+                print("Gonna try again.")
+                # self.ikreq.seed_mode = 2
+                # self.flag = 1
+            return
+
+def main():
+    ik_class = IK_solver()
+
+    #define desired pose
+    pose = rospy.Subscriber('/z_controls/object_pose', Pose, ik_class.callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
